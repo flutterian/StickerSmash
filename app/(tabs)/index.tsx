9a915372cb206a7258a4894783764bca1d0d8f56
BYUNGMIN CHOI <bmchoi803@gmail.com>
2025-10-23 06:23:02 +0900

    Button 컴포넌트 적용

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,20 @@
 import { StyleSheet, View } from 'react-native';
 
-import ImageViewer from '@/components/imageViewer';
+import Button from '@/components/Button';
+import ImageViewer from '@/components/ImageViewer';
 
 
-const PlaceholderImage = require('@/assets/images/background-image.png');
-
+const PlaceholderImage = require("@/assets/images/background-image.png");
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo" />
+        <Button label="Use this photo" />
       </View>
     </View>
   );
@@ -24,10 +28,10 @@ const styles = StyleSheet.create({
   },
   imageContainer: {
     flex: 1,
+    paddingTop: 28,
   },
-  image: {
-    width: 320,
-    height: 440,
-    borderRadius: 18,
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
